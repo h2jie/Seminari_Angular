7f7fb53e94165b4d8ed1e8ceb52efbddaefcd8ec
h2jie <h2jie09@gmail.com>
2025-03-18 20:50:42 +0100

    Entrega seminari angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { UsuarioComponent } from './usuario/usuario.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'usuario', component: UsuarioComponent }
+];
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -20,4 +20,9 @@ export class UserService {
   getUser(id: number): Observable<User>{
     return this.http.get<User>(this.apiUrl+"/"+id);
   }
+  
+
+  registerUser(user: User): Observable<User> {
+    return this.http.post<User>(this.apiUrl, user);
+  }
 }
